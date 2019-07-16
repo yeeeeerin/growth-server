47030d4867369efa702addef068640ee482544dc
yeeeeerin <hepari2154@naver.com>
2019-07-16 14:16:49 +0900

    feat 식물 카드, 식물 정보 dto에 설명 작성

--- src/main/java/com/example/growth/dto/PlantDto.java
+++ src/main/java/com/example/growth/dto/PlantDto.java
@@ -12,20 +12,26 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @ApiModel
 public class PlantDto {
+    @ApiModelProperty(notes="식물 이름")
     private String name;
 
+    @ApiModelProperty(notes="식물 종류")
     private String kind;
 
     //나이/단계/레벨
+    @ApiModelProperty(notes="나이/ 단계/ 레벨")
     private String grow;
 
-    @ApiModelProperty(dataType = "string", allowableValues = "FLOWER, TREE, DRUPE, WILD_FLOWER, HERB")
+    @ApiModelProperty(dataType = "string",notes = "일러스트 종류", allowableValues = "FLOWER, TREE, DRUPE, WILD_FLOWER, HERB")
     private PlantTypes card;
 
+    @ApiModelProperty(notes="물 주는 주기")
     private Integer waterDate;
 
+    @ApiModelProperty(notes="물 주는 시간")
     private LocalDateTime waterTime;
 
+    @ApiModelProperty(notes="알람 여부")
     private Boolean alarm;
 
 
